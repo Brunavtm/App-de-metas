ba86565cc959867785792ff94ae7bfd7e526b3f9
Bruna <brunavtm@gmail.com>
2024-09-12 20:52:33 -0300

    metas abertas

--- app/index.js
+++ app/index.js
@@ -27,14 +27,14 @@ const listarMetas = async () => {
         instructions: false,
     })
 
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
     if(respostas.length == 0 ){
         console.log("Nenhuma meta foi selecionada.")
         return
     }
-
-    metas.forEach((m) => {
-        m.checked = false
-    })
 
     respostas.forEach((resposta) => {
         const meta = metas.find((m) => {
@@ -58,8 +58,24 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas: " + realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+    })
+
+    if (abertas.length == 0){
+        console.log('Não existem metas abertas! :)')
+        return
+    }
+
+    await select({
+        message: "Metas abertas: " + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -77,6 +93,10 @@ const iniciar = async () => {
             {
                 name: "Listar metas",
                 value: "listar"
+            },
+            {
+                name: "Metas Abertas",
+                value: "abertas"
             },
             {
                 name: "Metas Realizadas",
@@ -101,6 +121,9 @@ const iniciar = async () => {
                 break
             case 'realizadas':
                 await metasRealizadas()
+                break
+            case 'abertas':
+                await metasAbertas()
                 break
             case 'sair':
                 console.log("Até logo!")
