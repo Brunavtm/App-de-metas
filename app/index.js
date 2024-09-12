f57c27ff2660bed058380f85b0bc6a55a05f64d0
Bruna <brunavtm@gmail.com>
2024-09-12 19:44:12 -0300

    Listar metas

--- app/index.js
+++ app/index.js
@@ -1,4 +1,51 @@
-const { select } = require('@inquirer/prompts');
+const { select, input, checkbox } = require('@inquirer/prompts');
+
+let meta1 = {
+    value: "Tomar 3L de água por dia.",
+    checked: false,
+}
+
+let metas = [meta1]
+
+const cadastrarMeta = async () => {
+    const meta = await input({message: "Informe a sua meta:"})
+
+    if(meta.length == 0){
+        console.log("A meta não foi digitada.")
+        return
+    }
+
+    metas.push(
+        {value: meta, checked: false}
+    )
+}
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as seta para se movimentar, espaço para marcar/desmarcar e enter para selecionar.",
+        choices: [...metas],
+        instructions: false,
+    })
+
+    if(respostas.length == 0 ){
+        console.log("Nenhuma meta foi selecionada.")
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    console.log("Metas atualizadas.")
+}
 
 const iniciar = async () => {
     
@@ -19,18 +66,18 @@ const iniciar = async () => {
                 name: "Sair",
                 value: "sair"
             }
-        ]
-
+        ],
             
 
         })
 
         switch(opcao){
             case 'cadastrar':
-                console.log("Cadastrar meta:")
+                await cadastrarMeta()
+                console.log(metas)
                 break
             case 'listar':
-                console.log("Listar meta:")
+                await listarMetas()
                 break
             case 'sair':
                 console.log("Até logo!")
